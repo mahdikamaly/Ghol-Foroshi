062ca62de32816abd48390ac3dd280d041a6df12
Wotox2 <MAHDIKAMALY20@GMAIL.COM>
2022-09-05 14:35:13 +0430

    Category Page Added

--- js/main-page/main-page-slider.js
+++ js/main-page/main-page-slider.js
@@ -6,3 +6,12 @@ $('.recent-products-body').slick({
     prevArrow: '<iconify-icon icon="akar-icons:chevron-left"></iconify-icon>',
     nextArrow: '<iconify-icon icon="akar-icons:chevron-right"></iconify-icon>',
 });
+
+$('.category-posts-body').slick({
+    arrows: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    prevArrow: '<iconify-icon icon="akar-icons:chevron-left"></iconify-icon>',
+    nextArrow: '<iconify-icon icon="akar-icons:chevron-right"></iconify-icon>',
+});
